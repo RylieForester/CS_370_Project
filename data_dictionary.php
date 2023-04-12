67070833da253846cabd4e7c65e7b99a7deb837e
RylieForester <rforester@mail.bradley.edu>
2023-04-12 12:43:44 -0500

    Updated data_dictionary.php and erd.php

--- data_dictionary.php
+++ data_dictionary.php
@@ -1,181 +1,313 @@
 <?php include_once("header.php")?>
 
+<style>
+    table{
+        border: 4px solid white;
+        background-color: rgb(94, 165, 212);
+        margin-left: auto;
+        margin-right: auto;
+        height: 200px;
+    }
+
+    .tableHeader{
+        border: 4px solid white;
+        font-size: 24px;
+        text-align: center;
+        height: 60px;
+        padding: 15px;
+    }
+
+    th{
+        font-size: 18px;
+        padding-top: 20px;
+        padding-bottom: 20px;
+        padding-left: 20px;
+    }
+</style>
+
 <div class="p-5 bg-dark">
     <h1 class = "text-light" style="text-align: center; text-decoration: underline">Data Dictionary</h1>
-    <p class = "text-light" style="text-align: center"> Below is a data dictionary that describes the attributes
-        within each table in our database in alphabetical order.</p>
     <br>
-
-    <table style="border: 1px solid white; background-color: rgb(94, 165, 212); margin-left: auto; margin-right: auto; height: 200px">
+    <p class = "text-light" style="text-align: center"> Below is an alphabetically ordered data dictionary that describes the attributes
+        within each table in our database.</p>
+    <br>
+    
+    <table>
         <tr>
-            <th style="border: 1px solid white; font-size: 22px; text-align: center; height: 40px">CART</th>
+            <th class="tableHeader">CART</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900 Customer_ID - The ID of the customer.
+            <th>&#9900 Customer_ID - The ID of the customer.
                 This value is a foreign key from the Customer_ID column in the Customer table.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900 Item_Number - The number associated with an item.
+            <th>&#9900 Item_Number - The number associated with an item.
                 This value is a foreign key from the Item_Number in the Items table. </th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px; padding-bottom: 15px">&#9900 Quantity_of_Item - The quantity of each item in the customer’s cart.
+            <th>&#9900 Quantity_of_Item - The quantity of each item in the customer’s cart.
                 Valid values are any integers 1 or greater. Functionally determined by Customer_ID and Item_Number.</th>
         </tr>
     </table>
     <br><br>
 
-    <table style="border: 1px solid white; background-color: rgb(94, 165, 212); margin-left: auto; margin-right: auto; height: 200px">
+    <table>
         <tr>
-            <th style="border: 1px solid white; font-size: 22px; text-align: center; height: 40px">CATEGORIES</th>
+            <th class="tableHeader">CATEGORIES</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900 Category_ID - The ID associated with the category.
+            <th>&#9900 Category_ID - The ID associated with the category.
                 Valid values are any integer 0 or greater. This value must be unique and is auto-incrementing.
                 It functionally determines all the non-key attributes in the Category table. This value is a primary key of the table.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900 Category_Desc - The description of the category.
+            <th>&#9900 Category_Desc - The description of the category.
                 Valid values are strings of up to 255 characters. Functionally determined by Category_ID. </th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px; padding-bottom: 15px">&#9900 Category_type - The type of the category.
+            <th>&#9900 Category_type - The type of the category.
                 Valid values are any valid category names. Functionally determined by Category_ID.</th>
         </tr>
     </table>
     <br><br>
 
-    <table style="border: 1px solid white; background-color: rgb(94, 165, 212); margin-left: auto; margin-right: auto; height: 200px">
+    <table>
         <tr>
-            <th style="border: 1px solid white; font-size: 22px; text-align: center; height: 40px">CUSTOMER</th>
+            <th class="tableHeader">CUSTOMER</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900  Customer_ID - The ID of the customer. Valid values are any integer 0 or greater.
+            <th>&#9900  Customer_ID - The ID of the customer. Valid values are any integer 0 or greater.
                 This value must be unique and is auto-incrementing. It functionally determines all the non-key attributes in the Customer table.
                 This value is a primary key of the table. </th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900  First_name - The first name of the customer. Valid values are any valid name.
+            <th>&#9900  First_name - The first name of the customer. Valid values are any valid name.
                 Functionally determined by Customer_ID.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900  Last_name - The last name of the customer. Valid values are any valid name.
+            <th>&#9900  Last_name - The last name of the customer. Valid values are any valid name.
                 Functionally determined by Customer_ID.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900  Age - The age of the customer. Valid values are any integer 0 or greater.
+            <th>&#9900  Age - The age of the customer. Valid values are any integer 0 or greater.
                 Functionally determined by Customer_ID.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900  Pref_gender - The preferred gender of the customer. Valid values are “Male” and “Female.”
+            <th>&#9900  Pref_gender - The preferred gender of the customer. Valid values are “Male” and “Female.”
                 Functionally determined by Customer_ID. </th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900  Phone_number - The phone number of the customer.
+            <th>&#9900  Phone_number - The phone number of the customer.
                 Valid values are any string of numbers that have a length of ten. Functionally determined by Customer_ID.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900  Address - The address of the customer. Valid values are any valid address.
+            <th>&#9900  Address - The address of the customer. Valid values are any valid address.
                 Functionally determined by Customer_ID.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900  Email - The email of the customer. Valid values are any valid emails.
+            <th>&#9900  Email - The email of the customer. Valid values are any valid emails.
                 Functionally determined by Customer_ID.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900  Username - The username of the customer.
+            <th>&#9900  Username - The username of the customer.
                 Valid values are any valid usernames. Functionally determined by Customer_ID.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px; ; padding-bottom: 15px">&#9900  Password - The password of the customer.
+            <th>&#9900  Password - The password of the customer.
                 Valid values are any valid passwords. Functionally determined by Customer_ID.</th>
         </tr>
     </table>
     <br><br>
 
-    <table style="border: 1px solid white; background-color: rgb(94, 165, 212); margin-left: auto; margin-right: auto; height: 200px">
+    <table>
         <tr>
-            <th style="border: 1px solid white; font-size: 22px; text-align: center; height: 40px">FAVORITES</th>
+            <th class="tableHeader">FAVORITES</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900 Customer_ID - The ID of the customer.
+            <th>&#9900 Customer_ID - The ID of the customer.
                 This value is a foreign key from the Customer_ID column in the Customer table.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900 Item_Number - The number associated with an item.
+            <th>&#9900 Item_Number - The number associated with an item.
                 This value is a foreign key from the Item_Number in the Items table.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px; padding-bottom: 15px">&#9900 Date_Added - The date an item was added favorited by a customer.
+            <th>&#9900 Date_Added - The date an item was added favorited by a customer.
                 Valid values are any valid dates. Functionally determined by Customer_ID.</th>
         </tr>
     </table>
     <br><br>
 
-    <table style="border: 1px solid white; background-color: rgb(94, 165, 212); margin-left: auto; margin-right: auto; height: 200px">
+    <table>
         <tr>
-            <th style="border: 1px solid white; font-size: 22px; text-align: center; height: 40px">ITEMS</th>
+            <th class="tableHeader">ITEMS</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900 Item_Number - The ID associated with the item. Valid values are any integer 0 or greater.
+            <th>&#9900 Item_Number - The ID associated with the item. Valid values are any integer 0 or greater.
                 This value must be unique and is auto-incrementing. It functionally determines all the non-key attributes in the Items table.
                 This value is a primary key of the table.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900 Item_Name - The name of the item.
+            <th>&#9900 Item_Name - The name of the item.
                 Valid values are any valid name. Functionally determined by Item_Number.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900 Price - The price of the item.
+            <th>&#9900 Price - The price of the item.
                 Valid values are any numbers that have a length of eight, with two digits after the decimal point.
                 Functionally determined by Item_Number. </th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900 Total_in_Stock - The total amount of the item that is in stock.
+            <th>&#9900 Total_in_Stock - The total amount of the item that is in stock.
                 Valid values are any integer 0 or greater.  Functionally determined by Item_Number.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900 Category_ID - The ID of the item category.
+            <th>&#9900 Category_ID - The ID of the item category.
                 This value is a foreign key from the Category_ID column in the Category table. Functionally determined by Item_Number. </th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900 Item_Description - The description of the item. Valid values are any string of up to 255 characters.
+            <th>&#9900 Item_Description - The description of the item. Valid values are any string of up to 255 characters.
                 Functionally determined by Item_Number.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px; padding-bottom: 15px">&#9900 Manufacturer - The manufacturer of the item. Valid values are any valid manufacturer name.
+            <th>&#9900 Manufacturer - The manufacturer of the item. Valid values are any valid manufacturer name.
                 Functionally determined by Item_Number. </th>
         </tr>
     </table>
     <br><br>
 
-    <table style="border: 1px solid white; background-color: rgb(94, 165, 212); margin-left: auto; margin-right: auto; height: 200px">
+    <table>
         <tr>
-            <th style="border: 1px solid white; font-size: 22px; text-align: center; height: 40px">ORDER</th>
+            <th class="tableHeader">ORDER</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900 Order_ID - The ID associated with a customer’s order.
+            <th>&#9900 Order_ID - The ID associated with a customer’s order.
                 Valid values are any integer 0 or greater. This value must be unique and is auto-incrementing.
                 It functionally determines all the non-key attributes in the Customer table. This value is a primary key of the table.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900 Shipping_Addr - The shipping address listed for the order.
+            <th>&#9900 Shipping_Addr - The shipping address listed for the order.
                 Valid values are any valid address. Functionally determined by Order_ID.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px">&#9900 Billing_Addr - The billing address listed for the order.
+            <th>&#9900 Billing_Addr - The billing address listed for the order.
                 Valid values are any valid address. Functionally determined by Order_ID.</th>
         </tr>
         <tr>
-            <th style="padding-top: 15px; padding-left: 10px; padding-bottom: 15px">&#9900 Est_Delivery_Date - The established delivery date of the order.
+            <th>&#9900 Est_Delivery_Date - The established delivery date of the order.
                 Valid values are any valid dates. Functionally determined by Order_ID.</th>
         </tr>
     </table>
     <br><br>
 
+    <table>
+        <tr>
+            <th class="tableHeader">ORDER DETAIL</th>
+        </tr>
+        <tr>
+            <th>&#9900 Order Detail_ID - The ID associated with a customer’s order details.
+                Valid values are any integer 0 or greater. This value must be unique and is auto-incrementing.
+                It functionally determines all the non-key attributes in the Order Detail table.
+                This value is a primary key of the table.</th>
+        </tr>
+        <tr>
+            <th>&#9900 Order_ID - The ID associated with a customer’s order.
+                This value is a foreign key from the Order_ID in the Order table.</th>
+        </tr>
+        <tr>
+            <th>&#9900 Item_Number - The number associated with an item.
+                This value is a foreign key from the Item_Number in the Items table.</th>
+        </tr>
+        <tr>
+            <th>&#9900 Quantity_of_Item - The quantity of each item in the customer’s order.
+                Valid values are any integers 1 or greater. Functionally determined by Order Detail_ID and Order_ID.</th>
+        </tr>
+        <tr>
+            <th>&#9900 Cost - The cost of an item in the order.
+                Valid values are any numbers that have a length of eight, with two digits after the decimal point.
+                Functionally determined by Order Detail_ID and Order_ID.. </th>
+        </tr>
+        <tr>
+            <th>&#9900 Total Amount - The total amount of the order, which is the sum of all item costs in the order.
+                Valid values are any numbers that have a length of eight, with two digits after the decimal point. Functionally determined by Order Detail_ID and Order_ID.</th>
+        </tr>
     </table>
+    <br><br>
 
+    <table>
+        <tr>
+            <th class="tableHeader">ORDER PAYMENT</th>
+        </tr>
+        <tr>
+            <th>&#9900 Order_ID - The ID associated with a customer’s order.
+                This value is a foreign key from the Order_ID in the Order table.</th>
+        </tr>
+        <tr>
+            <th>&#9900 Customer_ID - The ID of the customer.
+                This value is a foreign key from the Customer_ID column in the Customer table.</th>
+        </tr>
+        <tr>
+            <th>&#9900 Amount - The total amount of the order,
+                which is the sum of all item costs in the order plus any tax or shipping costs.
+                Valid values are any numbers that have a length of eight, with two digits after the decimal point. Functionally determined by Order Detail_ID and Order_ID.</th>
+        </tr>
+    </table>
+    <br><br>
 
+    <table>
+        <tr>
+            <th class="tableHeader">PAYMENT METHODS</th>
+        </tr>
+        <tr>
+            <th>&#9900 Customer_ID - The ID of the customer.
+                This value is a foreign key from the Customer_ID column in the Customer table.</th>
+        </tr>
+        <tr>
+            <th>&#9900 Payment_type - The type of payment method of the customer. Valid values are “Credit” and  “Debit.”
+                Functionally determined by Customer_ID.</th>
+        </tr>
+        <tr>
+            <th>&#9900 Card_number - The card number associated with the payment type of the customer.
+                Valid values are any valid card numbers that have a length of 16. Functionally determined by Customer_ID.</th>
+        </tr>
+    </table>
+    <br><br>
+
+    <table>
+        <tr>
+            <th class="tableHeader">PREVIOUS PURCHASES</th>
+        </tr>
+        <tr>
+            <th>&#9900 Customer_ID - The ID of the customer.
+                This value is a foreign key from the Customer_ID column in the Customer table.</th>
+        </tr>
+        <tr>
+            <th>&#9900 Order_number - The order number of a customer’s previous purchase.
+                Valid values are any valid order numbers. Functionally determined by Customer_ID.</th>
+        </tr>
+        <tr>
+            <th>&#9900 Date_added - The date of when the order was purchased.
+                Valid vales are any valid date. Functionally determined by Customer_ID. </th>
+        </tr>
+    </table>
+    <br><br>
+
+    <table>
+        <tr>
+            <th class="tableHeader">REVIEWS</th>
+        </tr>
+        <tr>
+            <th>&#9900 Customer_ID - The ID of the customer.
+                This value is a foreign key from the Customer_ID column in the Customer table.</th>
+        </tr>
+        <tr>
+            <th>&#9900 Item_Number - The number associated with an item.
+                This value is a foreign key from the Item_Number in the Items table.</th>
+        </tr>
+        <tr>
+            <th>&#9900 Date_added - The date an item was added favorited by a customer.
+                Valid values are any valid dates. Functionally determined by Customer_ID. </th>
+        </tr>
+    </table>
 </div>
 
 <?php include_once("footer.php")?>
--- erd.php
+++ erd.php
@@ -3,11 +3,10 @@
 <div class="p-5 bg-dark">
     <h1 class = "text-light" style="text-align: center; text-decoration: underline">Entity Relationship Diagram (ERD)</h1>
     <br>
-    <p class = "text-light" style="text-align: center"> Below is an graphical image of the various entities, attributes,
+    <p class = "text-light" style="text-align: center"> Below is a graphical image of the various entities, attributes,
         and relationships that are included in our database.</p>
+    <br>
     <p style="text-align: center"><img src="CS370_ERD.png" alt="Team #1 ERD (CS 370)"></p>
-
-
 </div>
 
 <?php include_once("footer.php")?>
