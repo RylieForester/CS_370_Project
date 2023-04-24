6069684b281940ccc3bd7f1e8a621fc10d5bbbeb
SoloShemp <100331830+SoloShemp@users.noreply.github.com>
2023-04-23 19:56:23 -0500

    Update data_import_2.php

--- data_import_2.php
+++ data_import_2.php
@@ -27,7 +27,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
                 $parsed_csv_line = str_getcsv($line);
 
-                //Do something with the parsed data . Look at the array (skip the first line).
+                //TODO: Do something with the parsed data . Look at the array (skip the first line).
                 $Category_ID = $parsed_csv_line[0];
                 $Category_Desc = $parsed_csv_line[1];
                 $Category_type = $parsed_csv_line[2];
@@ -122,6 +122,87 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 $parsed_csv_line_2 = str_getcsv($prev_line);
                 $prev_Category_Desc = $parsed_csv_line_2[1];
                 $prev_Item_name = $parsed_csv_line_2[4];
+
+                if ($line === $lines[array_key_last($lines)]) {
+                    ?> <div class = "p-5 bg-dark"> <?php
+                    $result = $connection->query("SELECT * FROM mydb.Categories");
+
+                    $query = array();
+                    while($query[] = mysqli_fetch_assoc($result));
+                    array_pop($query);
+
+                    echo '<table border="1">';
+                    echo '<tr>';
+                    foreach($query[0] as $key => $value) {
+                        echo '<td>';
+                        echo $key;
+                        echo '</td>';
+                    }
+                    echo '</tr>';
+                    foreach($query as $row) {
+                        echo '<tr>';
+                        foreach($row as $column) {
+                            echo '<td>';
+                            echo $column;
+                            echo '</td>';
+                        }
+                        echo '</tr>';
+                    }
+                    echo '</table>';
+
+                    $result2 = $connection->query("SELECT * FROM mydb.Items");
+
+                    $query2 = array();
+                    while($query2[] = mysqli_fetch_assoc($result2));
+                    array_pop($query2);
+
+                    echo '<table border="1">';
+                    echo '<tr>';
+                    foreach($query2[0] as $key => $value) {
+                        echo '<td>';
+                        echo $key;
+                        echo '</td>';
+                    }
+                    echo '</tr>';
+                    foreach($query2 as $row) {
+                        echo '<tr>';
+                        foreach($row as $column) {
+                            echo '<td>';
+                            echo $column;
+                            echo '</td>';
+                        }
+                        echo '</tr>';
+                    }
+                    echo '</table>';
+
+                    $result3 = $connection->query("SELECT * FROM mydb.Reviews");
+
+                    $query3 = array();
+                    while($query3[] = mysqli_fetch_assoc($result3));
+                    array_pop($query3);
+
+                    echo '<table border="1">';
+                    echo '<tr>';
+                    foreach($query3[0] as $key => $value) {
+                        echo '<td>';
+                        echo $key;
+                        echo '</td>';
+                    }
+                    echo '</tr>';
+                    foreach($query3 as $row) {
+                        echo '<tr>';
+                        foreach($row as $column) {
+                            echo '<td>';
+                            echo $column;
+                            echo '</td>';
+                        }
+                        echo '</tr>';
+                    }
+                    echo '</table>';
+
+                    echo "There were " . $count_of_Inserts . " rows inserted and " . $count_of_Updates . " rows updated";
+    ?> </div> <?php
+                }
 
             }
             mysqli_close($connection);
