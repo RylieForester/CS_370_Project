c9cc3f9f383b58a88fca467a4bd31693182bb65b
SoloShemp <100331830+SoloShemp@users.noreply.github.com>
2023-04-23 19:56:01 -0500

    Update data_import_1.php

--- data_import_1.php
+++ data_import_1.php
@@ -1,4 +1,6 @@
-<?php include_once("header.php") ?>
+<?php
+
+include_once("header.php") ?>
 
 <?php
 
@@ -27,7 +29,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
                 $parsed_csv_line = str_getcsv($line);
 
-                //Do something with the parsed data . Look at the array (skip the first line).
+                //TODO: Do something with the parsed data . Look at the array (skip the first line).
                 $Order_ID = $parsed_csv_line[0];
                 $Shipping_Addr = $parsed_csv_line[1];
                 $Billing_Addr = $parsed_csv_line[2];
@@ -114,7 +116,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $count_of_Inserts++;
                         }
                     }
-                    
+
                 }
                 $count++;
 
@@ -126,7 +128,89 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 $prev_Order_Detail_ID = $parsed_csv_line_2[5];
                 $prev_Order_ID = $parsed_csv_line_2[0];
 
-            }
+                if ($line === $lines[array_key_last($lines)]) {
+                    ?> <div class = "p-5 bg-dark"> <?php
+                    $result = $connection->query("SELECT * FROM mydb.Order");
+
+                    $query = array();
+                    while($query[] = mysqli_fetch_assoc($result));
+                    array_pop($query);
+
+                    echo '<table border="1">';
+                    echo '<tr>';
+                    foreach($query[0] as $key => $value) {
+                        echo '<td>';
+                        echo $key;
+                        echo '</td>';
+                    }
+                    echo '</tr>';
+                    foreach($query as $row) {
+                        echo '<tr>';
+                        foreach($row as $column) {
+                            echo '<td>';
+                            echo $column;
+                            echo '</td>';
+                        }
+                        echo '</tr>';
+                    }
+                    echo '</table>';
+
+                    $result2 = $connection->query("SELECT * FROM mydb.Order_Detail");
+
+                    $query2 = array();
+                    while($query2[] = mysqli_fetch_assoc($result2));
+                    array_pop($query2);
+
+                    echo '<table border="1">';
+                    echo '<tr>';
+                    foreach($query2[0] as $key => $value) {
+                        echo '<td>';
+                        echo $key;
+                        echo '</td>';
+                    }
+                    echo '</tr>';
+                    foreach($query2 as $row) {
+                        echo '<tr>';
+                        foreach($row as $column) {
+                            echo '<td>';
+                            echo $column;
+                            echo '</td>';
+                        }
+                        echo '</tr>';
+                    }
+                    echo '</table>';
+
+                    $result3 = $connection->query("SELECT * FROM mydb.Order_Payment");
+
+                    $query3 = array();
+                    while($query3[] = mysqli_fetch_assoc($result3));
+                    array_pop($query3);
+
+                    echo '<table border="1">';
+                    echo '<tr>';
+                    foreach($query3[0] as $key => $value) {
+                        echo '<td>';
+                        echo $key;
+                        echo '</td>';
+                    }
+                    echo '</tr>';
+                    foreach($query3 as $row) {
+                        echo '<tr>';
+                        foreach($row as $column) {
+                            echo '<td>';
+                            echo $column;
+                            echo '</td>';
+                        }
+                        echo '</tr>';
+                    }
+                    echo '</table>';
+
+                    echo "There were " . $count_of_Inserts . " rows inserted and " . $count_of_Updates . " rows updated";
+    ?> </div> <?php
+                }
+
+
+                }
 
             $connection->close();
             $import_succeeded = true;
