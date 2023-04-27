00069ca2f225d542bc15bf0ef609863c7557eeaf
tringuyen0208 <h004.thanhhd1@gmail.com>
2023-04-26 21:09:29 -0500

    data reports

--- data_report.php
+++ data_report.php
@@ -50,10 +50,72 @@ function output_error($title, $error){
                 echo "<tr><th>Customer ID</th><th>Name</th><th>Email</th></tr>";
 
                 $sql = "SELECT * FROM customers";
+
+                //second report
+                $con = @mysqli_connect("host", "username", "password", "database");
+                //retrieve data
+                $sql2 = "SELECT c.Category_name, i.Item_name, r.Review_text
+                    FROM categories c
+                    INNER JOIN items i 
+                        ON c.Category_ID = i.Category_ID
+                    LEFT OUTER JOIN reviews r 
+                        ON i.Item_Number = r.Item_Number";
+                $result2 = mysqli_query($con, $sql2);
+
+                // Display the report
+                echo '<table>';
+                echo '<thead>';
+                echo '<tr>';
+                echo '<th>Category Name</th>';
+                echo '<th>Item Name</th>';
+                echo '<th>Review Text</th>';
+                echo '</tr>';
+                echo '</thead>';
+                echo '<tbody>';
+                while ($row = mysqli_fetch_assoc($result2))
+                {
+                    echo '<tr>';
+                    echo '<td>' . $row['Category_name'] . '</td>';
+                    echo '<td>' . $row['Item_name'] . '</td>';
+                    echo '<td>' . $row['Review_text'] . '</td>';
+                    echo '</tr>';
+                }
+                echo '</tbody>';
+                echo '</table>';
+
+                //third report
+                $sql3 = "SELECT c.First_name, c.Last_name, ca.Cart_id, p.Payment_type
+                        FROM customers c
+                        LEFT OUTER JOIN carts ca 
+                            ON c.Customer_ID = ca.Customer_ID
+                        LEFT OUTER JOIN payment_methods p 
+                            ON ca.Customer_ID = p.Customer_ID";
+                $result3 = mysqli_query($con, $sql3);
+
+                //Display report
+                echo '<table>';
+                echo '<thead>';
+                echo '<tr>';
+                echo '<th>Customer Name</th>';
+                echo '<th>Cart ID</th>';
+                echo '<th>Payment Method Name</th>';
+                echo '</tr>';
+                echo '</thead>';
+                echo '<tbody>';
+                while ($row = mysqli_fetch_assoc($result3)) {
+                    echo '<tr>';
+                    echo '<td>' . $row['customer_name'] . '</td>';
+                    echo '<td>' . $row['cart_id'] . '</td>';
+                    echo '<td>' . $row['payment_method_name'] . '</td>';
+                    echo '</tr>';
+                }
+                echo '</tbody>';
+                echo '</table>';
+
+
             }
         }
     ?>
 
 </body>
-
 </html>
